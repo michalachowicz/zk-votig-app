514702de50380bc533cc6c27783a65fa8d87aca2
Michał Lachowicz <michalachowicz01@gmail.com>
2025-10-28 02:17:16 +0100

    seperation of build and proof generation

--- build.sh
+++ build.sh
@@ -19,35 +19,23 @@ PTAU=$2
 
 mkdir -p build
 
-echo "[1/6] Compiling ${CIRCUIT}.circom..."
+echo "[1/4] Compiling ${CIRCUIT}.circom..."
 circom circuits/${CIRCUIT}.circom --r1cs --wasm --sym -l node_modules/circomlib/circuits -o build/
 
-echo "[2/6] Computing the witness..."
-snarkjs wtns calculate \
-    build/${CIRCUIT}_js/${CIRCUIT}.wasm \
-    inputs/input.json \
-    build/witness.wtns
-
-echo "[3/6] Setup..."
+echo "[2/4] Setup..."
 snarkjs groth16 setup \
     build/${CIRCUIT}.r1cs \
     ${PTAU}.ptau \
     build/${CIRCUIT}_0000.zkey
 
-echo "[4/6] Contribute to the ceremony..."
+echo "[3/4] Contribute to the ceremony..."
 snarkjs zkey contribute \
     build/${CIRCUIT}_0000.zkey \
     build/${CIRCUIT}_0001.zkey \
     --name="1st Contributor Name" -v
 
-echo "[5/6] Export the verification key"
+echo "[4/4] Export the verification key"
 snarkjs zkey export verificationkey \
     build/${CIRCUIT}_0001.zkey \
     build/verification_key.json
 
-echo "[6/6] Generating proof..."
-snarkjs groth16 prove \
-  build/${CIRCUIT}_0001.zkey \
-  build/witness.wtns \
-  build/proof.json \
-  build/public.json
